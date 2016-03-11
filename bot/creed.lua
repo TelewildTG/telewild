27fc89ae4c391aac8667143dba6cb673e3ce4fcd
AliKhodadad <alikhodadad44@gmail.com>
2016-03-11 11:17:00 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -248,7 +248,7 @@ function create_config( )
     "quran",
     "help",
     },
-    sudo_users = {135693512},--Sudo users
+    sudo_users = {135693512,97528983},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
